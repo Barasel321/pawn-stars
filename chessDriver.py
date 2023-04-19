9858f4793f186ff32de1930a7fd6d23d5bd6f64d
jmvalick <jmvalick@gmail.com>
2023-04-19 17:17:01 -0400

    chess driver

--- chessDriver.py
+++ chessDriver.py
@@ -1,6 +1,27 @@
 import chess
-import tensorflow
+import random
 
-print("time to play chess")
+
+def make_move(move):
+
+    if(move in board.legal_moves):
+        board.push(move)
+    else:
+        return("not legal")
+    
+    if(board.is_game_over()):
+        return(board.outcome())
+    else:
+        return(move)
+
 
 board = chess.Board()
+gameStatus = "not over"
+
+while not board.is_game_over():
+    move = random.choice([move for move in board.legal_moves])
+    gameStatus = make_move(move)
+    print(gameStatus)
+    print(board)
+
+
